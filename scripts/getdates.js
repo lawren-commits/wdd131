b88d23f93a260abd65d385dba53dabfb7bc1f328
Lawren <lawren@sdrspero.com>
2026-09-08 08:55:18 +1300

    Fix last modified date text formatting

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,7 @@
+// Current Year
 const year = document.querySelector("#currentyear");
 year.textContent = new Date().getFullYear();
 
+// Last Modified Date
 const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = `Last Modification: ${document.lastModified}`;
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
