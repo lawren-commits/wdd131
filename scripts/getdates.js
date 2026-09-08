dfc4992923a2b54d0898c7a4934cbdbdd54bcb7f
Lawren <lawren@sdrspero.com>
2026-09-08 13:46:59 +1300

    Delete scripts directory

--- scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// Current Year
-const year = document.querySelector("#currentyear");
-year.textContent = new Date().getFullYear();
-
-// Last Modified Date
-const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
