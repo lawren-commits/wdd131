879d879e939e897b4f94d4c84a03b9ec700f5cd1
Lawren <lawren@sdrspero.com>
2026-09-09 08:54:16 +1300

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,1 @@
-const currentYear = new Date().getFullYear();
 
-document.getElementById("currentyear").textContent = currentYear;
-
-document.getElementById("lastModified").textContent =
-    "Last Modified: " + document.lastModified;
